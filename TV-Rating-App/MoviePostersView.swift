344e9d7b2232b9859166cf5118dab8b96b1d20ef
Luc Rieffel <65513046+lucrieffel@users.noreply.github.com>
2023-12-07 12:57:42 -0800

    Update

--- TV-Rating-App/MoviePostersView.swift
+++ TV-Rating-App/MoviePostersView.swift
@@ -10,17 +10,29 @@ import SwiftUI
 
 struct MoviePostersView: View {
     var posters = ["forest_gump_poster", "pulp_fiction_poster", "s_list", "The_Godfather_poster", "the_shawshank_redemption_poster"]
-
+    
     var body: some View {
         ScrollView (.horizontal){
             HStack {
-                Text("Movies").font(.title)
-                ForEach(posters, id: \.self) { posterName in
-                    Image(posterName)
-                        .resizable()
-                        .frame(width: 200, height: 200)
-                        .padding()
+                VStack{
+                    Text("Movies").font(.title)
+                    ForEach(posters, id: \.self) { posterName in
+                        //add posters who are movies in this section
+                        Image(posterName)
+                            .resizable()
+                            .frame(width: 200, height: 200)
+                            .padding()
+                    }
+                    Text("TV Shows").font(.title)
+                    ForEach(posters, id: \.self) { posterName in
+                        //add posters who are TV Shows here
+                        Image(posterName)
+                            .resizable()
+                            .frame(width: 200, height: 200)
+                            .padding()
+                    }
                 }
+                
             }
         }
         .navigationBarTitle("Recommended Movies", displayMode: .inline)
--- TV-Rating-App/Multimedia.swift
+++ TV-Rating-App/Multimedia.swift
@@ -26,6 +26,8 @@ struct Multimedia: Codable, Identifiable{
     var userRating: Double //Users numerical/float rating 1.0-5.0 of the media
     
     var imageName: String = "movie_clips" // name of the image in "Assets", default is a blank image
+    
+    var isWatched: Bool = false
 
 //
 //    var mediaContentRating: String //PG-13, R, etc.
