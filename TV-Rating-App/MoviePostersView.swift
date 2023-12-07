6c995e94b7d9352408e5e4ed3ff08bb227042ad7
Luc Rieffel <65513046+lucrieffel@users.noreply.github.com>
2023-12-07 13:04:18 -0800

    Update MoviePostersView.swift

--- TV-Rating-App/MoviePostersView.swift
+++ TV-Rating-App/MoviePostersView.swift
@@ -12,9 +12,10 @@ struct MoviePostersView: View {
     var posters = ["forest_gump_poster", "pulp_fiction_poster", "s_list", "The_Godfather_poster", "the_shawshank_redemption_poster"]
     
     var body: some View {
+        //Add Title for Recommended Movies
         ScrollView (.horizontal){
             HStack {
-                VStack{
+                HStack{
                     Text("Movies").font(.title)
                     ForEach(posters, id: \.self) { posterName in
                         //add posters who are movies in this section
