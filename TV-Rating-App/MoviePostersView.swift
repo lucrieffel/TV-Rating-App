88675af1e001d778a63754c53a79e51af6a54f19
Luc Rieffel <65513046+lucrieffel@users.noreply.github.com>
2023-12-10 15:33:57 -0800

    Update MoviePostersView.swift

--- TV-Rating-App/MoviePostersView.swift
+++ TV-Rating-App/MoviePostersView.swift
@@ -17,60 +17,67 @@ struct MoviePostersView: View {
     private var recommendedTVShows: [Multimedia] {
         multimediaStore.multimedias.filter { $0.isRecommended && $0.isTVShow }
     }
-
     var body: some View {
         VStack {
-            // Heading for the Recommended section
+            // Main Heading
             Text("WatchRater")
-                .font(.largeTitle)
-                .padding(5)
-            
+                .font(.system(size: 36, weight: .bold, design: .rounded))
+                .foregroundColor(.blue)
+                .padding(.top, 20)
+
             Text("Recommended For You")
-                .font(.title)
-                .padding(5)
-            Spacer()
+                .font(.system(size: 24, weight: .medium, design: .serif))
+                .padding(.bottom, 80)
+            
+
             // Movies Section
             SectionHeading(title: "Movies")
-            ScrollView(.horizontal) {
-                HStack {
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 15) {
                     ForEach(recommendedMovies, id: \.id) { media in
                         Image(media.imageName)
                             .resizable()
                             .scaledToFit()
                             .frame(width: 200, height: 200)
-                            .padding()
+                            .cornerRadius(10)
+                            .shadow(radius: 5)
                     }
                 }
+                .padding(.horizontal)
             }
             // TV Shows Section
             SectionHeading(title: "TV Shows")
-            ScrollView(.horizontal) {
-                HStack {
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 15) {
                     ForEach(recommendedTVShows, id: \.id) { media in
                         Image(media.imageName)
                             .resizable()
                             .scaledToFit()
                             .frame(width: 200, height: 200)
-                            .padding()
+                            .cornerRadius(10)
+                            .shadow(radius: 5)
                     }
                 }
+                .padding(.horizontal)
             }
         }
         .navigationBarTitle("Recommended Movies", displayMode: .inline)
+        .padding(.bottom, 10)
     }
 }
-
-//Create a structure to add a heading to the view
+//structure to create section heading for movies and shows
 struct SectionHeading: View {
     var title: String
     
     var body: some View {
         Text(title)
-            .font(.title)
+            .font(.system(size: 22, weight: .semibold, design: .rounded)) // Change the size as needed
             .padding(.vertical, 5)
+            .frame(maxWidth: .infinity, alignment: .center)
+            .padding(.horizontal, 10)
+            .underline()
     }
 }
-
 //Show Preview
 #Preview {
     MoviePostersView(multimediaStore: MultimediaStore(multimedias: MultimediaData))
